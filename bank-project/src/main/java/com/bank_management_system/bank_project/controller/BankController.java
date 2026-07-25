2ca60ff1025b5e1969acbad757a275ce814c4ed9
Rachit Nag <rachitknag@gmail.com>
2026-07-25 10:38:30 +0530

    Business logic from BankController is transfered to BankService.

--- bank-project/src/main/java/com/bank_management_system/bank_project/controller/BankController.java
+++ bank-project/src/main/java/com/bank_management_system/bank_project/controller/BankController.java
@@ -3,7 +3,6 @@ package com.bank_management_system.bank_project.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,54 +26,25 @@ public class BankController {
 	//1. Create Bank
 	@PostMapping
 	public ResponseEntity<ResponseStructure<Bank>> createBank(@RequestBody Bank bank) {
-		Bank savedBank = bankService.createBank(bank);
-	    
-	    ResponseStructure<Bank> res = new ResponseStructure<>();
-	    res.setData(savedBank);
-	    res.setStatusCode(HttpStatus.CREATED.value());
-	    res.setMessage("Bank created successfully!");
-		
-		return new ResponseEntity<ResponseStructure<Bank>>(res, HttpStatus.CREATED);
+		return bankService.createBank(bank);	
 	}
 	
 	//2. Get All Bank
 	@GetMapping("/all")
 	public ResponseEntity<ResponseStructure<List<Bank>>> getAllBank() {
-		List<Bank> fetchedBanks = bankService.getAllBank();
-		
-		ResponseStructure<List<Bank>> res = new ResponseStructure<List<Bank>>();
-		res.setData(fetchedBanks);
-		res.setStatusCode(HttpStatus.OK.value());
-		res.setMessage("All Bank Records fetched.");
-		
-		return new ResponseEntity<ResponseStructure<List<Bank>>>(res, HttpStatus.OK);
+		return bankService.getAllBank();
 	}
 	
 	//3. Get Bank By Id
 	@GetMapping("/{bankId}")
 	public ResponseEntity<ResponseStructure<Bank>> getBankById(@PathVariable Integer bankId) {
-		Bank fetchBank = bankService.getBankById(bankId);
-		
-		ResponseStructure<Bank> res = new ResponseStructure<Bank>();
-		res.setData(fetchBank);
-		res.setStatusCode(HttpStatus.OK.value());
-		res.setMessage("Bank record fetched.");
-		
-		return new ResponseEntity<ResponseStructure<Bank>>(res, HttpStatus.OK);
-		
+		return bankService.getBankById(bankId);
 	}
 	
 	//4. Delete Bank
 	@DeleteMapping("/{bankId}")
 	public ResponseEntity<ResponseStructure<Bank>> deleteBankRecordById(@PathVariable Integer bankId) {
-		Bank deletedBank = bankService.deleteBankById(bankId);
-		
-		ResponseStructure<Bank> res = new ResponseStructure<Bank>();
-		res.setData(deletedBank);
-		res.setStatusCode(HttpStatus.OK.value());
-		res.setMessage("Bank record with id "+bankId+" deleted successfully.");
-		
-		return new ResponseEntity<ResponseStructure<Bank>>(res,HttpStatus.OK);
+		return bankService.deleteBankById(bankId);
 	}
 	
 	//5.1 Update Bank -> PutMapping
@@ -93,28 +63,14 @@ public class BankController {
 	//7. Get By IFSC
 	@GetMapping("/ifsc/{ifsc}")
 	public ResponseEntity<ResponseStructure<Bank>> getBankByIfsc(@PathVariable String ifsc) {
-		Bank fetchedBank = bankService.getBankByIfsc(ifsc);
-		
-		ResponseStructure<Bank> res = new ResponseStructure<Bank>();
-		res.setData(fetchedBank);
-		res.setStatusCode(HttpStatus.OK.value());
-		res.setMessage("Bank recorded fetched successfully.");
-		
-		return new ResponseEntity<ResponseStructure<Bank>>(res, HttpStatus.OK);
+		return bankService.getBankByIfsc(ifsc);
 	}
 	
 	
 	//8. Get Bank By Address Id
 	@GetMapping("/address/{addressId}")
 	public ResponseEntity<ResponseStructure<Bank>> getBankByAddressId(@PathVariable Integer addressId) {
-		Bank fetchedBank = bankService.getBankByAddressId(addressId);
-		
-		ResponseStructure<Bank> res = new ResponseStructure<Bank>();
-		res.setData(fetchedBank);
-		res.setStatusCode(HttpStatus.OK.value());
-		res.setMessage("Bank record fetched using addressId: "+addressId);
-		
-		return new ResponseEntity<ResponseStructure<Bank>>(res, HttpStatus.OK);
+		return bankService.getBankByAddressId(addressId);
 	}
 	
 	//9. Get Bank By Address
@@ -122,26 +78,12 @@ public class BankController {
 	//10. Get Bank By City
 	@GetMapping("/city/{city}")
 	public ResponseEntity<ResponseStructure<List<Bank>>> getBankByCity(@PathVariable String city) {
-		List<Bank> fetchedBanksList = bankService.getBankByCity(city);
-		
-		ResponseStructure<List<Bank>> res = new ResponseStructure<List<Bank>>();
-		res.setData(fetchedBanksList);
-		res.setStatusCode(HttpStatus.OK.value());
-		res.setMessage("Bank records fetched in city "+city);
-		
-		return new ResponseEntity<ResponseStructure<List<Bank>>>(res, HttpStatus.OK);
+		return bankService.getBankByCity(city);
 	}
 	
 	//11. Get Bank By Contact Number
 	@GetMapping("/contact/{contactNo}")
 	public ResponseEntity<ResponseStructure<Bank>> getBankByContactNo(@PathVariable String contactNo) {
-		Bank fetchedBank = bankService.getBankByContactNo(contactNo);
-		
-		ResponseStructure<Bank> res = new ResponseStructure<Bank>();
-		res.setData(fetchedBank);
-		res.setStatusCode(HttpStatus.OK.value());
-		res.setMessage("Bank with contact Number "+contactNo+" fetched successfully.");
-		
-		return new ResponseEntity<ResponseStructure<Bank>>(res, HttpStatus.OK);
+		return bankService.getBankByContactNo(contactNo);
 	}
 }
--- bank-project/src/main/java/com/bank_management_system/bank_project/service/BankService.java
+++ bank-project/src/main/java/com/bank_management_system/bank_project/service/BankService.java
@@ -1,4 +1,5 @@
 package com.bank_management_system.bank_project.service;
+import com.bank_management_system.bank_project.dto.ResponseStructure;
 import com.bank_management_system.bank_project.entity.Bank;
 import com.bank_management_system.bank_project.exception.DuplicateResourceException;
 import com.bank_management_system.bank_project.exception.InvalidDataException;
@@ -8,6 +9,8 @@ import com.bank_management_system.bank_project.repository.BankRepository;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,7 +21,7 @@ public class BankService {
 	private BankRepository bankRepository;
 
 	
-	public Bank createBank(Bank bank) {
+	public ResponseEntity<ResponseStructure<Bank>> createBank(Bank bank) {
 
 	    if (bank.getBankId() != null) {
 	        throw new InvalidDataException("Bank Id should not be provided for creation");
@@ -35,69 +38,119 @@ public class BankService {
 	    if (bank.getAddress() != null) {
 	        bank.getAddress().setBank(bank);
 	    }
-	   
-	    return bankRepository.save(bank);
+	 
+	    Bank savedBank = bankRepository.save(bank);
+	    
+	    ResponseStructure<Bank> res = new ResponseStructure<>();
+	    res.setData(savedBank);
+	    res.setStatusCode(HttpStatus.CREATED.value());
+	    res.setMessage("Bank created successfully!");
+	    
+	    return new ResponseEntity<ResponseStructure<Bank>>(res, HttpStatus.CREATED);
 	}
 
 
-	public List<Bank> getAllBank() {
-		return bankRepository.findAllWithAddress();
+	public ResponseEntity<ResponseStructure<List<Bank>>> getAllBank() {
+		
+		List<Bank> fetchedBanks = bankRepository.findAllWithAddress();
+		
+		ResponseStructure<List<Bank>> res = new ResponseStructure<List<Bank>>();
+		res.setData(fetchedBanks);
+		res.setStatusCode(HttpStatus.OK.value());
+		res.setMessage("All Bank Records fetched.");
+		
+		return new ResponseEntity<ResponseStructure<List<Bank>>>(res, HttpStatus.OK);
 	}
 
 
-	public Bank getBankById(Integer bankId) {
-		return bankRepository.findById(bankId)
+	public ResponseEntity<ResponseStructure<Bank>> getBankById(Integer bankId) {
+		Bank fetchBank = bankRepository.findById(bankId)
 				.orElseThrow(()->new ResourceNotFoundException("Bank record with id "+bankId+" doesn't exists"));
+		
+		ResponseStructure<Bank> res = new ResponseStructure<Bank>();
+		res.setData(fetchBank);
+		res.setStatusCode(HttpStatus.OK.value());
+		res.setMessage("Bank record fetched.");
+		
+		return new ResponseEntity<ResponseStructure<Bank>>(res, HttpStatus.OK);
 	}
 
 	@Transactional
-	public Bank deleteBankById(Integer bankId) {
+	public ResponseEntity<ResponseStructure<Bank>> deleteBankById(Integer bankId) {
 		
-		Bank deletedbank = bankRepository.findById(bankId)
+		Bank deletedBank = bankRepository.findById(bankId)
 				.orElseThrow(()->new ResourceNotFoundException("Bank record with id "+bankId+" doesn't exists."));
-		bankRepository.delete(deletedbank);
-		return deletedbank;
+		bankRepository.delete(deletedBank);
+		
+		ResponseStructure<Bank> res = new ResponseStructure<Bank>();
+		res.setData(deletedBank);
+		res.setStatusCode(HttpStatus.OK.value());
+		res.setMessage("Bank record with id "+bankId+" deleted successfully.");
+		
+		return new ResponseEntity<ResponseStructure<Bank>>(res,HttpStatus.OK);
 	}
 
 
-	public Bank getBankByIfsc(String ifsc) {
+	public ResponseEntity<ResponseStructure<Bank>> getBankByIfsc(String ifsc) {
 		if(ifsc==null) {
 			throw new InvalidDataException("The bank ifsc code is required to fetch.");
 		}
 		
 		Bank fetchedBank = bankRepository.findByIfsc(ifsc)
 				.orElseThrow(()->new ResourceNotFoundException("Bank record with ifsc "+ifsc+" doesn't exists."));
+	
+		ResponseStructure<Bank> res = new ResponseStructure<Bank>();
+		res.setData(fetchedBank);
+		res.setStatusCode(HttpStatus.OK.value());
+		res.setMessage("Bank recorded fetched successfully.");
 		
-		return fetchedBank;
+		return new ResponseEntity<ResponseStructure<Bank>>(res, HttpStatus.OK);
 	}
 
 
-	public Bank getBankByAddressId(Integer addressId) {
+	public ResponseEntity<ResponseStructure<Bank>> getBankByAddressId(Integer addressId) {
 		if(addressId == null) {
 			throw new InvalidDataException("AddressId is mandatory to fetch Bank by addressId.");
 		}
 		
-		return bankRepository.findByAddressId(addressId)
+		Bank fetchedBank = bankRepository.findByAddressId(addressId)
 				.orElseThrow(()->new ResourceNotFoundException("Bank record with addressId "+addressId+" doesn't exist."));
+	
+		ResponseStructure<Bank> res = new ResponseStructure<Bank>();
+		res.setData(fetchedBank);
+		res.setStatusCode(HttpStatus.OK.value());
+		res.setMessage("Bank record fetched using addressId: "+addressId);
+		
+		return new ResponseEntity<ResponseStructure<Bank>>(res, HttpStatus.OK);
 	}
 
 
-	public List<Bank> getBankByCity(String city) {
+	public ResponseEntity<ResponseStructure<List<Bank>>> getBankByCity(String city) {
 		List<Bank> fetchedBanksList = bankRepository.findByAddressWithCity(city);
 		
 		if(fetchedBanksList.isEmpty()) {
 			throw new ResourceNotFoundException("No Bank found in this city.");
 		}
+				
+		ResponseStructure<List<Bank>> res = new ResponseStructure<List<Bank>>();
+		res.setData(fetchedBanksList);
+		res.setStatusCode(HttpStatus.OK.value());
+		res.setMessage("Bank records fetched in city "+city);
 		
-		return fetchedBanksList;
+		return new ResponseEntity<ResponseStructure<List<Bank>>>(res, HttpStatus.OK);
 	}
 
 
-	public Bank getBankByContactNo(String contactNo) {
+	public ResponseEntity<ResponseStructure<Bank>> getBankByContactNo(String contactNo) {
 		Bank fetchedBank = bankRepository.findByContactNo(contactNo)
 				.orElseThrow(()->new ResourceNotFoundException("No Bank Record found with contact No "+contactNo));
 		
-		return fetchedBank;		
+		ResponseStructure<Bank> res = new ResponseStructure<Bank>();
+		res.setData(fetchedBank);
+		res.setStatusCode(HttpStatus.OK.value());
+		res.setMessage("Bank with contact Number "+contactNo+" fetched successfully.");
+		
+		return new ResponseEntity<ResponseStructure<Bank>>(res, HttpStatus.OK);
 	}
 
 
